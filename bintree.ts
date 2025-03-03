968f4ae2d710f40150fef4d0eac9175aa6217459
Santiago Andrade <seyacat@gmail.com>
2025-03-03 17:54:51 -0500

    Working nextindex

--- bintree.ts
+++ bintree.ts
@@ -1,12 +1,14 @@
 export class Bintree {
   root: BintreeNode;
+  indexRoot: BintreeNode;
   bits: number;
   constructor(bits: number, data?: bigint) {
     this.bits = bits;
     if (data) {
       this.loadData(data);
     } else {
-      this.root = new BintreeNode(null, null);
+      this.root = new BintreeNode();
+      this.indexRoot = new BintreeNode();
     }
   }
   loadData = (data: bigint) => {
@@ -40,17 +42,27 @@ export class Bintree {
           n[0] = (n[0] << 1n) | 1n;
           break;
       }
-      if (n[0].toString(2).length == this.bits + 1 ) {
+      if (n[0].toString(2).length == this.bits + 1) {
         const shifted = n.shift();
         if (shifted) {
           nums.push(shifted & ((1n << BigInt(this.bits)) - 1n));
         }
       }
     }
-    nums = [...nums , ...n].map(  (nn:bigint) => (nn & (1n << BigInt(this.bits)) - 1n) );
+    nums = [...nums, ...n].map(
+      (nn: bigint) => nn & ((1n << BigInt(this.bits)) - 1n)
+    );
   };
-  add = (n: bigint) => {
-    let cursor = this.root;
+  add = (n: bigint, index?: bigint) => {
+    console.log({ nav: this.nextAvailableIndex() });
+    index = index ?? this.nextAvailableIndex();
+    this._add(this.root, n);
+    this._add(this.indexRoot, index);
+    //console.dir({"in":this.indexRoot.toObject(100)},{depth:null})
+  };
+
+  _add = (rootNode: BintreeNode, n: bigint) => {
+    let cursor = rootNode;
     const bitLength = n.toString(2).length;
     let lastBit = n >> BigInt(this.bits - 1);
     for (let i = this.bits - 1; i >= 0; i--) {
@@ -58,20 +70,56 @@ export class Bintree {
       if (bit) {
         if (!cursor.t)
           cursor.t = new BintreeNode(
-            i == 0 && lastBit ? this.root.t : null,
-            i == 0 && !lastBit ? this.root.f : null
+            cursor
+            //i == 0 && lastBit ? this.root.t : null, //ROOT LOOP
+            //i == 0 && !lastBit ? this.root.f : null
           );
         cursor = cursor.t;
       } else {
         if (!cursor.f)
           cursor.f = new BintreeNode(
-            i == 0 && lastBit ? this.root.t : null,
-            i == 0 && !lastBit ? this.root.f : null
+            cursor
+            //i == 0 && lastBit ? this.root.t : null,
+            //i == 0 && !lastBit ? this.root.f : null
           );
         cursor = cursor.f;
       }
     }
   };
+
+  remove = (n: bigint) => {
+    const chain = this.bintreeChain(n);
+    if (chain.length !== this.bits) return;
+    for (let i = this.bits - 1; i >= 0; i--) {
+      if (chain[i - 1].t && chain[i - 1].t === chain[i]) {
+        chain[i - 1].t = null;
+        if (chain[i - 1].f) return;
+      }
+      if (chain[i - 1].f && chain[i - 1].f === chain[i]) {
+        chain[i - 1].f = null;
+        if (chain[i - 1].t) return;
+      }
+      return;
+    }
+  };
+  bintreeChain = (n: bigint): BintreeNode[] => {
+    let cursor = this.root;
+    const ret: BintreeNode[] = [];
+    for (let i = this.bits - 1; i >= 0; i--) {
+      const bit = (n >> BigInt(i)) & 1n;
+      if (bit) {
+        if (!cursor.t) return [];
+        ret.push(cursor.t);
+        cursor = cursor.t;
+      } else {
+        if (!cursor.f) return [];
+        ret.push(cursor.f);
+        cursor = cursor.f;
+      }
+    }
+    return ret;
+  };
+
   find = (n: bigint) => {
     let cursor = this.root;
     for (let i = this.bits - 1; i >= 0; i--) {
@@ -86,25 +134,71 @@ export class Bintree {
     }
     return true;
   };
-  toArray = () => {
-    return [
-      ...(this.root.t?.toArray(this.bits, 1n) ?? []),
-      ...(this.root.f?.toArray(this.bits, 0n) ?? []),
-    ].flat();
+  nextAvailableIndex = () => {
+    let cursor = this.indexRoot;
+    let index: bigint = 1n;
+    for (let i = 0n; i < this.bits; i++) {
+      
+      console.log("fulltest", i, cursor.f?.isFull(this.bits - Number(i) - 1));
+      if (
+        cursor.f &&
+        !cursor.t &&
+        (cursor.f.isFull(this.bits - Number(i) - 2) || i === BigInt(this.bits) - 1n)
+      ) {
+        console.log(
+          "condicion 1",
+          i,
+          BigInt(this.bits) - i - 1n,
+          JSON.stringify(cursor.toObject(100), null, 2)
+        );
+        index = (index << 1n) | 1n;
+        index = index << (BigInt(this.bits) - i - 1n);
+        break;
+      }
+      if (cursor.t) {
+        console.log("condicion 2");
+        index = (index << 1n) | 1n;
+        cursor = cursor.t;
+        continue;
+      }
+      if (cursor.f) {
+        console.log("condicion 3");
+        index = index << 1n;
+        cursor = cursor.f;
+        continue;
+      }
+      if (!cursor.f) {
+        console.log("condicion 4");
+        index = index << (BigInt(this.bits) - i);
+        break;
+      }
+      console.log("INDEXIN", index.toString(2));
+    }
+    console.log("INDEX", index.toString(2));
+    return index;
   };
   fromArray = (arr: bigint[]) => {
     for (let i = 0; i < arr.length; i++) {
       this.add(arr[i]);
     }
   };
+  toArray() {
+    return this._toArray(this.root);
+  }
+  indexToArray() {
+    return this._toArray(this.indexRoot);
+  }
+  _toArray = (rootNode: BintreeNode) => {
+    return [
+      ...(rootNode.t?.toArray(this.bits, 1n) ?? []),
+      ...(rootNode.f?.toArray(this.bits, 0n) ?? []),
+    ].flat();
+  };
   toBigInt = () => {
     return (this.root.toBigInt(this.bits, 1n) << 1n) | 1n; //WITH PREFIX 1 AND TRAILING 1
   };
   toObject = (i = this.bits) => {
-    const ret: any = {};
-    if (this.root.t) ret.t = this.root.t.toObject(i);
-    if (this.root.f) ret.f = this.root.f.toObject(i);
-    return ret;
+    return this.root.toObject(100);
   };
 }
 
@@ -116,13 +210,19 @@ export class WordTree {
       this.tree.add(BigInt(w.charCodeAt(i)));
     }
   }
-  toObject = () => this.tree.toObject();
+  toObject = () => this.tree.toObject(100);
 }
 
 export class BintreeNode {
+  p: BintreeNode | null;
   t: BintreeNode | null;
   f: BintreeNode | null;
-  constructor(t: BintreeNode | null, f: BintreeNode | null) {
+  constructor(
+    p: BintreeNode | null = null,
+    t: BintreeNode | null = null,
+    f: BintreeNode | null = null
+  ) {
+    this.p = p;
     this.t = t;
     this.f = f;
   }
@@ -153,5 +253,13 @@ export class BintreeNode {
       n = this.f.toBigInt(i - 1, n);
     }
     return n;
+  };
+  isFull = (i = 0): boolean => {
+    console.dir({ FULLIN: 1, i, ob: this.toObject(100) }, { depth: null });
+    return (
+      this.t != null &&
+      this.f != null &&
+      (i <= 0 || (this.t.isFull(i - 1) && this.f.isFull(i - 1)))
+    );
   };
 }
--- test/test.ts
+++ test/test.ts
@@ -5,29 +5,62 @@ const bt = new Bintree(8);
 //bt.add(10n);
 
 bt.add(0n);
+console.log(bt.indexToArray())
+
 //bt.add(1n);
 bt.add(5n);
+console.log(bt.indexToArray())
+
 bt.add(4n);
+console.log(bt.indexToArray())
+
+bt.add(8n);
+console.log(bt.indexToArray())
+
+bt.add(9n);
+console.log(bt.indexToArray())
+
+bt.add(9n);
+console.log(bt.indexToArray())
+
+
+
+
+
+/*bt.add(4n);
 //bt.add(3n);
 //bt.add(520n);
 bt.add(19n);
 //bt.add(88n);
-//bt.add(2n);
+bt.add(2n);
 //bt.add(21n);
 //bt.add(250n);
+//bt.remove(12n);
+bt.remove(5n);
+bt.remove(4n);
+bt.add(45n);
+bt.remove(44n);*/
 
 
-console.dir(bt.toObject(), { depth: null });
+//console.dir({"ob":bt.toObject()}, { depth: null });
 
-/*console.dir(
+/*const bt2 = new Bintree(8);
+bt2.add(25n);
+console.log(bt2.toArray())
+bt2.remove(25n);
+bt2.add(55n);
+console.log(bt2.toArray())
+
+console.dir(
   {
-    1: bt.find(1),
-    2: bt.find(2),
-    3: bt.find(3),
-    7: bt.find(7),
+    0: bt.find(0n),
+    19: bt.find(19n),
+    3: bt.find(3n),
+    7: bt.find(7n),
+    5: bt.find(5n),
   },
   { depth: null }
-);*/
+);
 
 console.dir(bt.toArray(), { depth: null });
 
@@ -37,3 +70,4 @@ const targetBt = new Bintree(8, data);
 
 console.dir(bt.toArray());
 
+*/
